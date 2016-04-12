2f34288c51f9a4bfeda5356e7ac02a90901c4e9f
Tony Moey <moeyster@gmail.com>
2016-04-12 13:34:29 +0800

    deleted logs from repo, emended env to allow uploads to s3 successful.

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,9 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "#{ENV['AWS_ACCESS_KEY_ID']}",
-      :aws_secret_access_key  => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
-      :region                 => 'us-west-2' # Change this for different AWS region. Default is 'us-east-1'
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      :region                 => ENV['S3_REGION'],
+      :path_style             => true # Change this for different AWS region. Default is 'us-east-1'
   }
-  config.fog_directory  = "#{ENV['AWS_S3_BUCKET']}"
+  config.fog_directory  = ENV['S3_BUCKET']
 end
